d27aac1665be6c8321001544347d04589a14b322
Tyler Slaughter <stslaug@gmail.com>
2025-07-04 15:42:13 -0400

    fix?

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    output: 'export',
     distDir: "build",
     images: {
+        unoptimized: true,
         // List of allowed image domains
         remotePatterns: [
             {
